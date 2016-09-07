f296c2000c71edad036e7b7ccba320ddb6c7b24c
weck <luvshri@foxmail.com>
2016-09-07 11:48:20 +0800

    add schedule tasks

--- src/main/java/com/saltyfish/LianyungangWaterConservationManagementApplication.java
+++ src/main/java/com/saltyfish/LianyungangWaterConservationManagementApplication.java
@@ -2,8 +2,10 @@ package com.saltyfish;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LianyungangWaterConservationManagementApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/saltyfish/configuration/ScheduleTask.java
@@ -0,0 +1,25 @@
+package com.saltyfish.configuration;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by weck on 16/9/7.
+ *
+ * 时钟任务
+ */
+
+@Component
+public class ScheduleTask {
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 60000)
+    public void reportCurrentTime() {
+        System.out.println("当前时间:" + dateFormat.format(new Date()) + ","
+                + "时间戳：" + System.currentTimeMillis());
+    }
+}
